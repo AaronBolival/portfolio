2630e12b0b7cbd93d54b5456260a3af8264bb54d
AaronBolival <aaronbolival@gmail.com>
2021-08-06 15:38:08 +0800

    Initial commit

---
+++ src/about.js
@@ -0,0 +1,49 @@
+// import React from "react";
+// import {
+//     Container,Grid,Toolbar,Box,Paper,useMediaQuery,
+//     FormControl,InputLabel,Input,FormHelperText,FormGroup,TextareaAutosize,TextField,
+//     AppBar,Button,Typography,Chip,zIndex
+//   } from '@material-ui/core/';
+// import Image from 'material-ui-image';
+
+
+
+// const About = () => {
+//     <Container className={classes.aboutContainer}> 
+//     <Grid lg={12} container display="flex" direction="row"   justifyContent="center">  
+//       <Grid container display="flex" direction="column" lg={3} xs={8} className={classes.sss}>
+//            <Image src={aaron} />
+//       </Grid>
+//       <Grid container display="flex" direction="column" lg={5} xs={11} >
+       
+//           <Typography item className={classes.headerContent} variant="h4">About me</Typography>
+//           <Box container p={5}>
+//              <Typography item className={classes.sss} variant="body2">
+//               Hi! I am Aaron, A BSIT College graduate from STI College with background in programming and design(Web, Mobile Application and Software/Desktop ).
+//             </Typography> 
+//           </Box>
+//           <Box container p={5}>
+//              <Typography item className={classes.sss} variant="body2">
+//               I'm 22 years old residing in NCR, Caloocan and currently looking for a job.
+//             </Typography> 
+//           </Box>
+//           <Box container p={5}>
+//             <Button
+//               variant="contained"
+//               color="secondary"
+//               size="large"
+//               className={classes.button}
+//               startIcon={<GetAppSharpIcon />}
+//             >
+//               Download my Resume
+//             </Button>
+//           </Box>
+       
+//       </Grid>
+   
+//     </Grid>
+//   </Container>
+// }
+
+
+// export default About;
